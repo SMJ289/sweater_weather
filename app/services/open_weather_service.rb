540fcfdcae303b8df0dcb3a5aec8bf549b3db9bb
Steven Meyers <sjjmeyers@gmail.com>
2020-06-07 17:40:39 -0600

    Create open weather service

---
+++ app/services/open_weather_service.rb
@@ -0,0 +1,22 @@
+class OpenWeatherService
+  def weather(location)
+    coordinates = Coordinates.new
+    
+    response = conn.get('data/2.5/onecall') do |req|
+      req.params['lat'] = coordinates.lat(location)
+      req.params['lon'] = coordinates.lon(location)
+      req.params['exclude'] = 'minutely'
+    end
+
+    JSON.parse(response.body, symbolize_names: true)
+  end
+
+  def conn
+    Faraday.new(
+      url: 'https://api.openweathermap.org/',
+      params: {appid: ENV['WEATHER_API_KEY']},
+      headers: {'Content-Type' => 'application/json'}
+    )
+  end
+
+end
