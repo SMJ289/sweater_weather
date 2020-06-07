74ad620e35565e293b5315445bb1f61b5120e875
Steven Meyers <sjjmeyers@gmail.com>
2020-06-07 09:34:51 -0600

    Add current_weather poro

---
+++ app/poro/current_weather.rb
@@ -0,0 +1,29 @@
+class CurrentWeather
+  attr_reader :current_temp,
+              :high,
+              :low,
+              :feels_like,
+              :humidity,
+              :sunrise,
+              :sunset,
+              :uv_index,
+              :visibility,
+              :type
+
+  def initialize(data)
+    @current_temp = data[:current][:temp]
+    @high = kelvin_to_fahrenheit(data[:daily].first[:temp][:max])
+    @low = kelvin_to_fahrenheit(data[:daily].first[:temp][:min])
+    @feels_like = kelvin_to_fahrenheit(data[:current][:feels_like])
+    @humidity = data[:current][:humidity]
+    @sunrise = data[:current][:sunrise]
+    @sunset = data[:current][:sunset]
+    @uv_index = data[:current][:uvi]
+    @visibility = data[:current][:visibility]
+    @type = data[:current][:weather].first[:main]
+  end
+
+  def kelvin_to_fahrenheit(temp)
+    (temp * 9/5 - 459.67).round
+  end
+end
