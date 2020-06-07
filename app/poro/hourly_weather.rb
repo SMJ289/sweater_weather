916c20ec979fe8940871277b6a362a34b5ab81ae
Steven Meyers <sjjmeyers@gmail.com>
2020-06-07 14:45:05 -0600

    Create hourly weather poro

---
+++ app/poro/hourly_weather.rb
@@ -0,0 +1,19 @@
+class HourlyWeather
+  attr_reader :time,
+              :temp,
+              :icon
+
+  def initialize(data)
+    @time = format_time(data[:dt])
+    @temp = kelvin_to_fahrenheit(data[:temp])
+    @icon = data[:weather].first[:icon]
+  end
+
+  def format_time(dt)
+    Time.at(dt).strftime('%I:%M:%S %p')
+  end
+
+  def kelvin_to_fahrenheit(temp)
+    (temp * 9/5 - 459.67).round
+  end
+end
