bec672822f96ab6776b1636a724d5b2c187f3134
Steven Meyers <sjjmeyers@gmail.com>
2020-06-07 17:41:11 -0600

    Refactor forcast controller

--- app/controllers/api/v1/forecast_controller.rb
+++ app/controllers/api/v1/forecast_controller.rb
@@ -1,36 +1,8 @@
 class Api::V1::ForecastController < ApplicationController
   def show
-    google_conn = Faraday.new(
-    url: 'https://maps.googleapis.com/',
-    params: {key: ENV['GOOGLE_API_KEY']},
-    headers: {'Content-Type' => 'application/json'}
-    )
-
-    google_resp = google_conn.get('maps/api/geocode/json') do |req|
-      req.params['address'] = params['location']
-    end
-
-    google_json = JSON.parse(google_resp.body, symbolize_names: true)
-    lat = google_json[:results].first[:geometry][:location][:lat]
-    lon = google_json[:results].first[:geometry][:location][:lng]
-
-    weather_conn = Faraday.new(
-    url: 'https://api.openweathermap.org/',
-    params: {appid: ENV['WEATHER_API_KEY']},
-    headers: {'Content-Type' => 'application/json'}
-    )
-
-    weather_resp = weather_conn.get('data/2.5/onecall') do |req|
-      req.params['lat'] = lat
-      req.params['lon'] = lon
-      req.params['exclude'] = 'minutely'
-    end
-
-    weather_json = JSON.parse(weather_resp.body, symbolize_names: true)
-
-    forecast = Forecast.new(weather_json)
-
+    search_results = WeatherSearch.new
+    weather = search_results.weather(params['location'])
+    forecast = Forecast.new(weather)
     render json: ForecastSerializer.new(forecast)
-
   end
 end
