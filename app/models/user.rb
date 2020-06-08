1a2ed9b7a16e7fc45ebd92c47349318fa9e15aff
Steven Meyers <sjjmeyers@gmail.com>
2020-06-08 17:46:03 -0600

    Encrypt user password and add API key gen

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,15 @@
 class User < ApplicationRecord
-  validates_uniqueness_of :email
-  validates_presence_of :password, :password_confirmation
+  validates :email, uniqueness: true, presence: true
+  validates_presence_of :password, require: true
+
+  has_secure_password
+  before_create :set_api_key
+
+  def set_api_key
+    self.api_key = generate_api_key
+  end
+
+  def generate_api_key
+    SecureRandom.base58(24)
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,7 +3,22 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   describe 'validations' do
     it {should validate_uniqueness_of :email}
+    it {should validate_presence_of :email}
     it {should validate_presence_of :password}
-    it {should validate_presence_of :password_confirmation}
+  end
+
+  describe 'instance methods' do
+    it '#generate_api_key' do
+      user = User.new(email: 'example@example.com', password: 'password')
+      expect(user.generate_api_key.length).to eq(24)
+    end
+  end
+
+  describe 'class methods' do
+    it '.set_api_key' do
+      user = User.new(email: 'example@example.com', password: 'password')
+      
+      expect(user.set_api_key.length).to eq(24)
+    end
   end
 end
