da4c94e4b550742f927bef797a929fc74fcdf9f4
Steven Meyers <sjjmeyers@gmail.com>
2020-06-08 15:12:31 -0600

    Refactor BackgroundController

--- app/controllers/api/v1/background_controller.rb
+++ app/controllers/api/v1/background_controller.rb
@@ -1,18 +1,7 @@
 class Api::V1::BackgroundController < ApplicationController
   def show
-    conn = Faraday.new(
-      url: 'https://api.unsplash.com/',
-      params: {client_id: ENV['UNSPLASH_API_KEY']},
-      headers: {'Content-Type' => 'application/json'}
-    )
-
-    resp = conn.get('search/photos') do |req|
-      req.params['query'] = params['location']
-    end
-
-    json = JSON.parse(resp.body, symbolize_names: true)
-    background_image = BackgroundImage.new(json)
+    search_results = BackgroundImageSearch.new
+    background_image = search_results.image(params['location'])
     render json: BackgroundImageSerializer.new(background_image)
-    binding.pry
   end
 end
