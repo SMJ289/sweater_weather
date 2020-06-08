347f127b298fe8f873431f71d5e40b7fe3d9b8f0
Steven Meyers <sjjmeyers@gmail.com>
2020-06-08 17:46:29 -0600

    Modify user table

--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_08_221024) do
+ActiveRecord::Schema.define(version: 2020_06_08_224406) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "users", force: :cascade do |t|
     t.string "email"
-    t.string "password"
-    t.string "password_confirmation"
+    t.string "password_digest"
     t.string "api_key"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
