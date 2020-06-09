356d6e2db2289dd01b1830511fbfb5601b758dca
Steven Meyers <sjjmeyers@gmail.com>
2020-06-09 16:03:49 -0600

    Refactor forecast_controller

--- app/controllers/api/v1/forecast_controller.rb
+++ app/controllers/api/v1/forecast_controller.rb
@@ -2,7 +2,6 @@ class Api::V1::ForecastController < ApplicationController
   def show
     search_results = WeatherSearch.new
     weather = search_results.weather(params['location'])
-    forecast = Forecast.new(weather)
-    render json: ForecastSerializer.new(forecast)
+    render json: ForecastSerializer.new(Forecast.new(weather))
   end
 end
