2858af870eae76d6a4824278fa8752c97b657c43
Steven Meyers <sjjmeyers@gmail.com>
2020-06-08 21:58:24 -0600

    Remove uneccessary global varible assignment

--- app/controllers/api/v1/user_controller.rb
+++ app/controllers/api/v1/user_controller.rb
@@ -1,11 +1,11 @@
 class Api::V1::UserController < ApplicationController
 
   def create
-    @user = User.new(user_params)
-    if @user.save
-      render json: UserSerializer.new(@user), status: 201
+    user = User.new(user_params)
+    if user.save
+      render json: UserSerializer.new(user), status: 201
     else
-      render json: {message: @user.errors.full_messages.to_sentence}, status: 400
+      render json: {message: user.errors.full_messages.to_sentence}, status: 400
     end
   end
 
