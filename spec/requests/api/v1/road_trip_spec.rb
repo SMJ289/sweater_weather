607296f3edf4a03aea66fa3f5f62bb9379dda288
Steven Meyers <sjjmeyers@gmail.com>
2020-06-09 14:25:11 -0600

    Add sad path testing for road_trip endpoint

--- spec/requests/api/v1/road_trip_spec.rb
+++ spec/requests/api/v1/road_trip_spec.rb
@@ -1,13 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe 'Road Trip API', :vcr do
-  it 'Registers User and returns response with 201 status' do
+  it 'returns roadtrip data if API key is valid' do
     user = User.create(email: 'email@example.com', password: 'password', password_confirmation: 'password')
     body = {
       'origin': 'Denver, CO',
       'destination': 'Pueblo, CO',
       'api_key': user.api_key
     }
+
     post '/api/v1/road_trip', params: body, as: :json
     
     expect(response).to be_successful
@@ -20,7 +21,32 @@ RSpec.describe 'Road Trip API', :vcr do
     expect(json[:data][:attributes][:travel_time]).to be_present
     expect(json[:data][:attributes][:arrival_temp]).to be_present
     expect(json[:data][:attributes][:arrival_weather_type]).to be_present
-    
+  end
+
+  it 'returns error if API key is invalid' do
+    user = User.create(email: 'email@example.com', password: 'password', password_confirmation: 'password')
+    body = {
+      'origin': 'Denver, CO',
+      'destination': 'Pueblo, CO',
+      'api_key': 'alskjdlaskjdalksjd'
+    }
+
+    post '/api/v1/road_trip', params: body, as: :json
+
+    expect(response.status).to eq(401)
+  end
+
+  it 'returns error if no API key is provided' do
+    user = User.create(email: 'email@example.com', password: 'password', password_confirmation: 'password')
+    body = {
+      'origin': 'Denver, CO',
+      'destination': 'Pueblo, CO',
+      'api_key': nil
+    }
+
+    post '/api/v1/road_trip', params: body, as: :json
+
+    expect(response.status).to eq(401)
   end
 
 end
