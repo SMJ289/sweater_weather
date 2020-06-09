fb19958264c8c2374817a5c2695b466ff18f33bd
Steven Meyers <sjjmeyers@gmail.com>
2020-06-08 20:09:25 -0600

    Add user registration sad path and test

--- app/controllers/api/v1/user_controller.rb
+++ app/controllers/api/v1/user_controller.rb
@@ -2,12 +2,11 @@ class Api::V1::UserController < ApplicationController
 
   def create
     @user = User.new(user_params)
-
     if @user.save
       render json: UserSerializer.new(@user), status: 201
-
+    else
+      render json: {status: 400, message: @user.errors.full_messages.to_sentence}, status: 400
     end
-
   end
 
   private
@@ -15,6 +14,4 @@ class Api::V1::UserController < ApplicationController
   def user_params
     params.permit(:email, :password, :password_confirmation)
   end
-  
-
 end
--- spec/requests/api/v1/user_registration_spec.rb
+++ spec/requests/api/v1/user_registration_spec.rb
@@ -8,13 +8,62 @@ RSpec.describe 'User Registration API', :vcr do
       'password_confirmation': 'password'
     }
     post '/api/v1/users', params: user_info, as: :json
+
     expect(response).to be_successful
     expect(response.status).to eq(201)
     
     user = User.last
     json = JSON.parse(response.body, symbolize_names: true)
-    binding.pry
+
     expect(json[:data][:attributes][:email]).to eq(user.email)
     expect(json[:data][:attributes][:api_key]).to eq(user.api_key)
+  end
+
+  it 'cannot register user when email is already taken' do
+    User.create(
+      email: 'email@example.com',
+      password: 'password',
+      password_confirmation: 'password'
+    )
+
+    user_info = {
+      'email': 'email@example.com',
+      'password': 'password',
+      'password_confirmation': 'password'
+    }
+    post '/api/v1/users', params: user_info, as: :json
+
+    expect(response.status).to eq(400)
+    
+    json = JSON.parse(response.body, symbolize_names: true)
+    expect(json[:message]).to eq("Email has already been taken")
+  end
+
+  it 'cannot register user when info is missing' do
+    user_info = {
+      'email': nil,
+      'password': nil,
+      'password_confirmation': nil
+    }
+    post '/api/v1/users', params: user_info, as: :json
+
+    expect(response.status).to eq(400)
+    
+    json = JSON.parse(response.body, symbolize_names: true)
+    expect(json[:message]).to eq("Email can't be blank, Password can't be blank, and Password can't be blank")
+  end
+
+  it 'cannot register user when passwords do not match' do
+    user_info = {
+      'email': 'example@example.com',
+      'password': 'cat',
+      'password_confirmation': 'dog'
+    }
+    post '/api/v1/users', params: user_info, as: :json
+
+    expect(response.status).to eq(400)
+    
+    json = JSON.parse(response.body, symbolize_names: true)
+    expect(json[:message]).to eq("Password confirmation doesn't match Password")
   end
 end
