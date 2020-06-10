c2f43acd8fb84ee9e76405771f4f547fbb6a2462
Steven Meyers <sjjmeyers@gmail.com>
2020-06-09 22:11:34 -0600

    Format for rubocop

--- app/controllers/api/v1/background_controller.rb
+++ app/controllers/api/v1/background_controller.rb
@@ -1,6 +1,7 @@
 class Api::V1::BackgroundController < ApplicationController
   def show
     return render json: { message: 'Location not given.' }, status: :bad_request if background_params[:location].blank?
+
     search_results = BackgroundImageSearch.new
     background_image = search_results.image(background_params[:location])
     render json: BackgroundImageSerializer.new(background_image)
