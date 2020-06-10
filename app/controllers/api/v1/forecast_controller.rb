c62b31eccfe33b32b337c728effb5013e0196a23
Steven Meyers <sjjmeyers@gmail.com>
2020-06-09 21:03:07 -0600

    Refactor to use strong params in all controllers

--- app/controllers/api/v1/forecast_controller.rb
+++ app/controllers/api/v1/forecast_controller.rb
@@ -1,7 +1,13 @@
 class Api::V1::ForecastController < ApplicationController
   def show
     search_results = WeatherSearch.new
-    weather = search_results.weather(params['location'])
+    weather = search_results.weather(forecast_params[:location])
     render json: ForecastSerializer.new(Forecast.new(weather))
+  end
+
+  private
+
+  def forecast_params
+    params.permit(:location)
   end
 end
--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,10 +1,16 @@
 class Api::V1::SessionsController < ApplicationController
   def create
-    user = User.find_by(email: params[:email])
-    if user.authenticate(params[:password])
+    user = User.find_by(email: user_params[:email])
+    if user.authenticate(user_params[:password])
       render json: UserSerializer.new(user), status: :ok
     else
       render json: { message: 'Invalid credentials.' }, status: :bad_request
     end
+  end
+
+  private
+
+  def user_params
+    params.permit(:email, :password)
   end
 end
